ca66f51045932b7c108ed14aea50f924b973713c
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-04-19 18:32:23 +1000

    update

---
+++ modules/panels/dynamics_365_query.js
@@ -0,0 +1,44 @@
+var dynamics_365_query=function(UID){
+    var t=sessionStorage.getItem("microsoft_oauth");
+    if(t==null){
+        alert("You haven't login!");
+        return "";
+    }
+    t=JSON.parse(t);
+    var access_token=t.a.access_token;
+    var dt=$vm.jwt_decode(access_token);
+    var exp=dt.exp;
+    if(Date.now() >= exp * 1000){
+        alert("Microsoft access token is expired! Please refresh.");
+        return;
+    }
+    $vm.request({cmd:"find",table:"microsoft-api",query:{UID:UID},options:{}},function(res){
+        var records=res.result;
+        if(records!=undefined && records.length==1){
+            var host='woolcock.crm6.dynamics.com';
+            var name=records[0].Data.Name;
+            var path=records[0].Data.Path;
+            var array=records[0].Data.Array;
+            var columns=records[0].Data.Columns;
+            path=path.replace(/ /g,'%20');
+            path=path.replace(/\n/g,'').replace(/\r/g,'');
+            $vm.request({cmd:'microsoft-api',access_token:access_token,host:host,path:path},function(res){
+                var v=res.result;
+                if(res.status=="err"){
+                    $vm.show_module('jsonv',{json_data:v})
+                }
+                else if(array!=""){
+                    //$vm.show_module('gridv',{name:name,json_data:JSON.parse(v),array:array,columns:columns})
+                    var type=columns.indexOf('|');
+                    if(type!=-1) $vm.show_module('gridv',{name:name,json_data:JSON.parse(v),array:array,columns:columns})
+                    else $vm.show_module('arrayv',{name:name,json_data:JSON.parse(v),array:array,columns:columns})
+}
+                else{
+                    $vm.show_module('jsonv',{json_data:JSON.parse(v)})
+                }
+            })
+        }
+        else alert('No report.')
+        console.log(res);
+    })
+}
